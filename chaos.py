236738ad1b94cf244466aeecbd744e5e035c4e1c
rebekahorth1 <rebekah.orth@gmail.com>
2015-09-16 18:08:41 +0000

    Finished assignment

--- chaos.py
+++ chaos.py
@@ -1,13 +1,17 @@
 __author__ = 'rebekah.orth'
+#Chaos.py
+#Rebekah Orth
+#CIS 125 Fall 2015
 
 # File: chaos.py
 # A simple program illustrating chaotic behavior.
-
+multiplier = 2.0
+loopcounter = 10
 def main():
     print("This program illustrates a chaotic function")
     x = eval(input("Enter a number between 0 and 1: "))
-    for i in range(10):
-        x = 3.9 * x * (1 - x)
+    for i in range(loopcounter):
+        x = multiplier * x * (1 - x)
         print(x)
 
 main()
